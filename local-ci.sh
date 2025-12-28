6cb8155007223dfaa5d699a41b65d70282d6d9d6
Robert King <varitonium@gmail.com>
2025-12-29 00:04:45 +0100

    XPY-31: upe-unit tests report generation

--- local-ci.sh
+++ local-ci.sh
@@ -3,7 +3,7 @@
 black --check --line-length 79 src \
 && pycodestyle src \
 && pydocstyle src
-coverage run -m pytest tests 
+coverage run -m pytest tests --html=report.html --self-contained-html
 coverage report -m
 coverage json
 coverage html
