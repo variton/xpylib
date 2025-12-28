3dd79de95607dd0efbb790743336dbd22e2f422f
Robert King <varitonium@gmail.com>
2025-12-29 00:25:44 +0100

    XPY-32: upe-linter adding

--- local-ci.sh
+++ local-ci.sh
@@ -2,7 +2,8 @@
 
 black --check --line-length 79 src \
 && pycodestyle src \
-&& pydocstyle src
+&& pydocstyle src \
+&& ruff check src --fix
 coverage run -m pytest tests --html=report.html --self-contained-html
 coverage report -m
 coverage json
